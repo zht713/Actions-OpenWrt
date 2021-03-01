c0ce4751c786192baeae3f43e8cf895186bf95ea
zht713 <54735954+zht713@users.noreply.github.com>
2021-03-01 23:49:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,7 +14,6 @@
 sed -i 's/^#\(.*helloworld\)/\1/' openwrt/feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' openwrt/feeds.conf.default
 sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' openwrt/feeds.conf.default
 
 # Update Luci
